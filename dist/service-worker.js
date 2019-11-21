35ec8ead57d364254a4ae8bc56c1f894dd33ff8e
Nand Kishor <nandkk07@gmail.com>
2019-11-21 22:30:53 +0530

    no changes

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.d755370532f9f0ec7764086c0668b811.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.b8fe209b6d0863bea49364ebca669c41.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
