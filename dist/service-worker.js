0e060c9dbfc779907b683f37829ace6dd7f3fc7e
Parshant <29176704+parshantsharma360@users.noreply.github.com>
2019-11-25 15:23:41 +0530

    Updated Location

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-importScripts("/precache-manifest.51e7c319c612bc30e7f4bb5438b30ee7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
-=======
-importScripts("/precache-manifest.afe7dcd0d95fee74edf17b8be9648939.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
->>>>>>> 9e47604ad20c4ad2e91e1bea3a9b90cfc6791d60
+importScripts("/precache-manifest.03f21a27418b61cc32b2fe4a7bb7a5f2.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
