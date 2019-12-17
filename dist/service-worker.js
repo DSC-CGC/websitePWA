dd5a98059d30a448ee05a13d64201b5788416ac2
mohitgupta004 <guptamohi.009@gmail.com>
2019-12-18 02:20:00 +0530

    Event

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.31f9f0e3e4e2c587eef3a1d6c9437660.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.973db27b1255d23758accf30bb36b360.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
